89a06eb353d3828b2c8ce345bb63e268b012c1ff
Zaregotto <Zaregotto307@gmail.com>
2021-11-11 19:28:09 +0200

    classwork

--- classwork/classwork.js
+++ classwork/classwork.js
@@ -1,31 +1,97 @@
-const arr = [50, 60, 30, 25, -98, 14, -228, 666, 1488, 1337];
-// const reducer = (previousValue, currentValue) => previousValue + currentValue;
-function reducer(previousValue, currentValue) {
-    // console.log(previousValue, currentValue)
-     return previousValue + currentValue
-};
-let sum = 0
-arr.forEach(function (item) {
-    sum += item
-    console.log(item)
-})
-console.log(sum)
-console.log(arr.reduce(reducer));
+// const arr = [50, 60, 90, -100, 200, 400, 500, -350, 250, 500];
+// const result = [Math.floor(Math.random()*100),Math.floor(Math.random()*10)];
+// // const full = arr.reduce((acc, arr) => {
+// //     acc += arr
+// //     return acc
+// // }, 0)
+// console.log(arr[0]+ arr[1]+ arr[2] + arr[3] + arr[4] + arr[5] + arr[6] + arr[7] + arr[7] + arr[8] + arr[9]);
+// console.log(result);
+// console.log(result[0]+ result[1] );
 
-// console.log(reducer(5, 10,))
+// const book = {
+//     name: 'mein kampf',
+//     pageCount: 1488,
+//     genre: 'phantasy'
+// }
+// console.log(book.name)
+//
+// const book1 = {
+//     name: 'mein kampf1',
+//     pageCount: 14881,
+//     genre: 'phantasy1',
+//     author: 'Adolf'
+// }
+//
+// let books = [
+//     {
+//         name: 'mein kampf',
+//         name: 'mein kampf1',
+//     },
+//     {
+//         pageCount: 1488,
+//         pageCount: 14881,
+//     },
+//     {
+//         genre: 'phantasy',
+//         genre: 'phantasy1',
+//     },
+//     {
+//         author: 'Adolf'
+//     },
+//
+// ];
+// console.log(books[1],)
 
-const book = {
-    name: 'mein kampf',
-    pagecount: 1488,
-    ganre: 'phantasy'
-}
-console.log(book.name)
 
-const books = [
-    {name: 'lalla',pagecount: 20, ganre: 'phentasy' },
-    {name:  'another name', jopa: 25,},
-    {}
-]
-books.forEach(function (book) { console.log(book) })
-console.log(books[1])
+let height = prompt ('введіть висоту')
+let width = prompt ('ведіть ширину')
+let s = height * width
+
+
+console.log('s =', s)
+
+
+// function Cylinder(cyl_height, cyl_diameter) {
+//     this.cyl_height = cyl_height;
+//     this.cyl_diameter = cyl_diameter;
+// }
+// Cylinder.prototype.Volume = function () {
+//     var radius = this.cyl_diameter / 4;
+//     return this.cyl_height * Math.PI * radius * radius;
+// };
+// var cyl = new Cylinder(10, 4);
+// // Volume of the cylinder with four decimal places.
+// console.log('volume =', cyl.Volume().toFixed(4));
+
+
+
+// let height = 10
+// let diameter = 4
+// let p = 3.14
+
+// let v = 2 * p * diameter * diameter * height
+//
+// console.log('v =', v)
+
+
+// let v = Math.pow(diameter,2)*p*height
+// match.sqrt(diameter)
+
+
+// console.log('v =', v)
+//
+//
+// let n = 3
+// let m = 4
+//
+// let k = Math.pow(n, 2) + Math.pow(m, 2);
+//
+// console.log('k= '+ Math.sqrt(k))
+
+
+
+
+
+
+
 
