d84410feb2147d47072d66b8cf0b355a9adfe563
Zaregotto <Zaregotto307@gmail.com>
2021-12-01 14:25:42 +0200

    homework 8

---
+++ homework8/main.js
@@ -0,0 +1,85 @@
+// - Напишіть код,  який за допомоги document.getElementById або document.getElementsByClassName або document.getElementsByTagName :
+
+
+
+
+// -- отримує текст з параграфа з id "content"
+// let text = document.getElementById ("content");
+// console.log(text);
+
+
+
+// -- отримує текст з блоку з id "rules"
+// let textBlock = document.getElementById('rules')
+// console.log(textBlock)
+
+
+// -- замініть текст параграфа з id 'content' на будь-який інший
+// let textReplace = document.getElementById('content');
+// console.log(textReplace);
+// textReplace.innerText = 'Я змінив текст я молодець'
+
+
+
+
+// -- замініть текст параграфа з id 'rules' на будь-який інший
+// let textInner = document.getElementById('rules')
+// console.log(textInner)
+// textInner.innerText = 'В мене точно не велике чсв'
+
+
+
+// -- змініть кожному елементу колір фону на червоний
+// let textBackgroundColor = document.body
+// console.log(textBackgroundColor)
+// textBackgroundColor[0].style.background = 'red'
+
+// let backgroundColor = document.getElementById('content')
+// console.log(backgroundColor)
+// backgroundColor.style.background = 'red'
+// let backgroundColorClass = document.getElementById('rules')
+// console.log(backgroundColorClass)
+// backgroundColorClass.style.background = 'red'
+// let backgroundColorRed = document.getElementsByClassName('fc_rules')
+// console.log(backgroundColorRed)
+// for (const li of backgroundColorRed) {
+//     li.style.background = 'red'
+// }
+
+// let all = document.querySelectorAll('*');
+// for (const elem of all) {
+//     elem.style.background = 'red'
+// }
+
+
+
+// -- змініть кожному елементу колір тексту на синій
+// let aColor = document.getElementById('content')
+// console.log(aColor)
+// aColor.style.color = 'blue'
+// let bColor = document.getElementById('rules')
+// console.log(bColor)
+// bColor.style.color = 'blue'
+// let colorBlue = document.getElementsByClassName('fc_rules')
+// console.log(colorBlue)
+// for (const li of colorBlue) {
+//     li.style.background = 'blue'
+// }
+
+
+
+// -- отримати весь список класів елемента з id=rules і вивести їх в console.log
+// let rule = document.getElementById('rules');
+// console.log(rule.classList)
+
+
+
+
+// -- поміняти колір тексту у всіх елементів fc_rules на червоний
+// let fc = document.getElementsByClassName('fc_rules')
+// for (const fcRul of fc) {
+//     fcRul.style.color = 'red'
+// }
+
+
+
